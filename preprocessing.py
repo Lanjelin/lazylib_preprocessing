e3d73ad1e02e2fef0b050ac630d09db1a9057b6e
Lanjelin <lanjelin.svanes@gmail.com>
2019-04-04 11:16:58 +0200

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+import os, subprocess, sys
+
+# should we sort and set track number?
+def getFiles(path):
+	files = []
+	# r=root, d=directories, f = files
+	for r, d, f in os.walk(path):
+	    for file in f:
+	        if '.mp3' in file:
+	            files.append(os.path.join(r, file))
+	return files
+
+def fixTags(files,author,book):
+	for file in files:
+		subprocess.call([
+          'kid3-cli',
+          '-c', 'set title ""',
+          '-c', 'set artist "'+author+'"',
+          '-c', 'set album "'+book+'"',
+          '-c', 'set song ""',
+          '-c', 'set track ""',
+          '-c', 'set comment ""',
+          '-c', 'set year ""',
+          '-c', 'set genre "183"',
+          '-c', 'set band ""',
+          '-c', 'set composer ""',
+          '-c', 'set copyright ""',
+          '-c', 'set url ""',
+          '-c', 'set publisher ""',
+          '-c', 'syncto 1',
+          file])
+
+authorname = ''
+bookname = ''
+if sys.argv[1] == 'test':
+    if '.py' in subprocess.check_output(['kid3-cli','-c', 'ls']):
+    	print 'preprocessing.py sucessfully installed'
+    else:
+    	print 'kid3-cli seems to be missing'
+else:
+    booktype = sys.argv[1]
+    bookfolder = sys.argv[2]
+    if len(sys.argv) == 5:
+        authorname = sys.argv[3]
+        bookname = sys.argv[4]
+	fixTags(getFiles(bookfolder),authorname,bookname)
